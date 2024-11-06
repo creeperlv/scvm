569e2d8db898b14bc4534b281c886b28f71fcc58
Creeper Lv <creeperlv@users.noreply.github.com>
2024-11-06 17:42:12 +1100

    Completed Arguments.cs.

--- scvm.net/samples/directvm/linker/directvm.linker.shell/Program.cs
+++ scvm.net/samples/directvm/linker/directvm.linker.shell/Program.cs
@@ -1,9 +1,15 @@
-﻿namespace DirectVM.linker.shell;
+﻿
+using scvm.tools.core;
+
+namespace DirectVM.linker.shell;
 
 class Program
 {
-    static void Main(string[] args)
-    {
-
-    }
+	static void Main(string[] args)
+	{
+		Arguments arguments = new Arguments();
+		arguments.Definitions.Add(new ArgumentDefinition("input", ["-i", "--input"]) { HasValues = true });
+		arguments.Definitions.Add(new ArgumentDefinition("output", ["-o", "--output"]) { HasValues = true });
+		arguments.Resolve(args);
+	}
 }
--- scvm.net/tools/core/scvm.tools.core/Arguments.cs
+++ scvm.net/tools/core/scvm.tools.core/Arguments.cs
@@ -1,40 +1,74 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 
 namespace scvm.tools.core
 {
 	public class Arguments
 	{
 		public List<string> SingleStringArgument = new List<string>();
-		public Dictionary<string, string> arguments = new Dictionary<string, string>();
-		public List<ArgumentWithParameter> Definitions = new List<ArgumentWithParameter>();
+		public Dictionary<string, string?> arguments = new Dictionary<string, string?>();
+		public List<ArgumentDefinition> Definitions = new List<ArgumentDefinition>();
 		public Arguments()
 		{
+		}
+		public bool TryFindDefinition(string name, [MaybeNullWhen(false)] out ArgumentDefinition definition)
+		{
+			foreach (ArgumentDefinition def in Definitions)
+			{
+				if (def.Names.Contains(name))
+				{
+					definition = def;
+					return true;
+				}
+			}
+			definition = null;
+			return false;
 		}
 		public void Resolve(string[] args)
 		{
 			for (int i = 0; i < args.Length; i++)
 			{
 				string arg = args[i];
-				foreach (ArgumentWithParameter parameter in Definitions)
+				if (TryFindDefinition(arg, out var definition))
 				{
-					if (parameter.Names.Contains(arg))
+					if (definition.HasValues)
 					{
-						if (parameter.HasValues)
+						i++;
+						if (i >= arg.Length)
 						{
+							arguments.Add(definition.id, null);
+							continue;
+						}
+						var next = args[i + 1];
+						if (TryFindDefinition(arg, out _))
+						{
+							arguments.Add(definition.id, null);
+						}
+						else
+						{
+							arguments.Add(definition.id, next);
 						}
 					}
+					else
+					{
+						arguments.Add(definition.id, null);
+					}
+				}
+				else
+				{
+					SingleStringArgument.Add(arg);
 				}
 			}
 		}
 	}
-	public class ArgumentWithParameter
+	public class ArgumentDefinition
 	{
 		public string id;
 		public List<string> Names;
 		public bool HasValues;
 		public bool IsValueOptional;
-		public ArgumentWithParameter(string id, List<string> names)
+		public ArgumentDefinition(string id, List<string> names)
 		{
 			this.id = id;
 			Names = names;
